2e2be33191a1f548c3bf231c8ea9dc92e258bc88
arad enoshi <aradenoshi10@gmail.com>
2025-11-18 14:36:16 +0200

    finish the table

--- aradmoran/JavaScript.js
+++ aradmoran/JavaScript.js
@@ -1,43 +1,54 @@
-﻿alert("something");
-document.write("something");
+﻿//alert("something");
+//document.write("something");
 
-document.write(" ");
+//document.write(" ");
 
-var num = prompt("enter a number", 7);
-document.write(num);
+//var num = prompt("enter a number", 7);
+//document.write(num);
 
-document.write(" ");
+//document.write(" ");
 
-var name = prompt("enter your name");
-document.write("hello " + name);
+//var name = prompt("enter your name");
+//document.write("hello " + name);
 
-document.write(" ");
+//document.write(" ");
 
-var age = parseInt(prompt("enter your age", 0));
-document.write("you are " + age + " years old");
+//var age = parseInt(prompt("enter your age", 0));
+//document.write("you are " + age + " years old");
 
-document.write("<br/>");
+//document.write("<br/>");
 
-document.write("next to" + age+ " " + "is"+ " " +(age+1));
+//document.write("next to" + age+ " " + "is"+ " " +(age+1));
 
-var num1 = parseInt(prompt("enter  num1"));
-var num2 = parseInt(prompt("enter  num2"));
+//var num1 = parseInt(prompt("enter  num1"));
+//var num2 = parseInt(prompt("enter  num2"));
 
-document.write("the sum " + (num1 + num2));
+//document.write("the sum " + (num1 + num2));
 
-document.write("<br/>");
+//document.write("<br/>");
 
-document.write("the machpela " + (num1 * num2));
-document.write("<br/>");
-if (num1 > num2)
-{
-    document.write("the efresh " + (num1 - num2));
+//document.write("the machpela " + (num1 * num2));
+//document.write("<br/>");
+//if (num1 > num2)
+//{
+//    document.write("the efresh " + (num1 - num2));
+//}
+//else if (num2 > num1)
+//{
+//    document.write("the efresh " + (num2 - num1));
+//}
+function CheckAnswer() {
+    var num = document.getElementById("num").value;
+    var num2 = document.getElementById("num2").value;
+    var num3 = num2 + num;
+   
 }
-else if (num2 > num1)
-{
-    document.write("the efresh " + (num2 - num1));
+function pool() {
+    var n1rnd = Math.floor(Math.random() * 10) + 1;
+    var n2rnd = Math.floor(Math.random() * 10) + 1;
+    document.getElementById("num").value = n1rnd;
+    document.getElementById("num2").value = n2rnd;
 }
-
 
 
 
