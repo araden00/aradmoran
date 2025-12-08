ddab58b02648ba11fc8400b4d231a8f76ff1a4f0
arad enoshi <aradenoshi10@gmail.com>
2025-12-08 21:40:16 +0200

    added machsevon ###

---
+++ aradmoran/Ex22.js
@@ -0,0 +1,6 @@
+﻿//targil1
+var div = document.getElementById("aElement");
+var aEl = document.createElement("a");
+aEl.setAttribute("href", "http://www.google.com");
+aEl.innerHTML = "google";
+div.appendChild(aEl);
--- aradmoran/JavaScript.js
+++ aradmoran/JavaScript.js
@@ -37,27 +37,105 @@
 //{
 //    document.write("the efresh " + (num2 - num1));
 //}
-function CheckAnswer() {
-    var num = parseInt(document.getElementById("num").value);
-    var num2 = parseInt(document.getElementById("num2").value);
-    var result = parseInt(document.getElementById("num3").value);
 
-    if (result = num2 + num); {
-        document.getElementById("span1").innerHTML = "כל הכבוד"
-    } 
-    else {
-        document.getElementById("span1").innerHTML = "טעות"
+//function GetAnswer() {
+//    var num = parseInt(document.getElementById("num").value);
+//    var num2 = parseInt(document.getElementById("num2").value);
+//    var correct = num + num2;
+//    document.getElementById("num3").value = correct;
+
+//    if (correct = num1 + num2); {
+//        document.getElementById("span").innerHTML = "מעולה"
+//    }
+//    if (correct = !num1 + num2) {
+//        document.getElementById("span").innerHTML = "טעות"
+//    }
+//}
+function CheckAnswer()
+{
+    var num = parseInt(document.getElementById("num+").value);
+    var num2 = parseInt(document.getElementById("num2+").value);
+    var userAnswer = parseInt(document.getElementById("num3+").value);
+    var correct = num + num2;
+    if (userAnswer == correct)
+    {
+        document.getElementById("span1+").value = "יפה מאוד ";
     }
-    
-    
-    
+    else
+    {
+        document.getElementById("span1+").value = "נסה שנית";
+    }
+
+    var num = parseInt(document.getElementById("num-").value);
+    var num2 = parseInt(document.getElementById("num2-").value);
+    var userAnswer = parseInt(document.getElementById("num3-").value);
+    var correct = num - num2;
+    if (userAnswer == correct) {
+        document.getElementById("span1-").value = "יפה מאוד ";
+    }
+    else {
+        document.getElementById("span1-").value = "נסה שנית";
+    }
+
+    var num = parseInt(document.getElementById("num*").value);
+    var num2 = parseInt(document.getElementById("num2*").value);
+    var userAnswer = parseInt(document.getElementById("num3*").value);
+    var correct = num * num2;
+    if (userAnswer == correct) {
+        document.getElementById("span1*").value = "יפה מאוד ";
+    }
+    else {
+        document.getElementById("span1*").value = "נסה שנית";
+    }
+
+    var num = parseInt(document.getElementById("num/").value);
+    var num2 = parseInt(document.getElementById("num2/").value);
+    var userAnswer = parseInt(document.getElementById("num3/").value);
+    var correct = num / num2;
+    if (userAnswer == correct) {
+        document.getElementById("span1/").value = "יפה מאוד ";
+    }
+    else {
+       document.getElementById("span1/").value = "נסה שנית";
+    }
+
+    var num = parseInt(document.getElementById("num%").value);
+    var num2 = parseInt(document.getElementById("num2%").value);
+    var userAnswer = parseInt(document.getElementById("num3%").value);
+    var correct = num % num2;
+    if (userAnswer == correct) {
+        document.getElementById("span1%").value = "יפה מאוד ";
+    }
+    else {
+        document.getElementById("span1%").value = "נסה שנית";
+    }
 }
-function pool() {
-    var n1rnd = Math.floor(Math.random() * 10) + 1;
-    var n2rnd = Math.floor(Math.random() * 10) + 1;
-    document.getElementById("num").value = n1rnd;
-    document.getElementById("num2").value = n2rnd;
-}
+
+
+
+    function pool() {
+        var n1rnd = Math.floor(Math.random() * 10) + 1;
+        var n2rnd = Math.floor(Math.random() * 10) + 1;
+        document.getElementById("num+").value = n1rnd;
+        document.getElementById("num2+").value = n2rnd;
+        var n3rnd = Math.floor(Math.random() * 10) + 1;
+        var n4rnd = Math.floor(Math.random() * 10) + 1;
+        document.getElementById("num-").value = n3rnd;
+        document.getElementById("num2-").value = n4rnd;
+        var n5rnd = Math.floor(Math.random() * 10) + 1;
+        var n6rnd = Math.floor(Math.random() * 10) + 1;
+        document.getElementById("num*").value = n5rnd;
+        document.getElementById("num2*").value = n6rnd;
+        var n7rnd = Math.floor(Math.random() * 10) + 1;
+        var n8rnd = Math.floor(Math.random() * 10) + 1;
+        document.getElementById("num/").value = n7rnd;
+        document.getElementById("num2/").value = n8rnd;
+        var n9rnd = Math.floor(Math.random() * 10) + 1;
+        var n10rnd = Math.floor(Math.random() * 10) + 1;
+        document.getElementById("num%").value = n9rnd;
+        document.getElementById("num2%").value = n10rnd;
+    }
+
 
 
 
