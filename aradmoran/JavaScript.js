c3dc73da69df674bf07f74410717baa3c04a3b9a
arad enoshi <aradenoshi10@gmail.com>
2025-12-08 21:51:02 +0200

    xxx

--- aradmoran/JavaScript.js
+++ aradmoran/JavaScript.js
@@ -8,49 +8,6 @@
 
 //document.write(" ");
 
-//var name = prompt("enter your name");
-//document.write("hello " + name);
-
-//document.write(" ");
-
-//var age = parseInt(prompt("enter your age", 0));
-//document.write("you are " + age + " years old");
-
-//document.write("<br/>");
-
-//document.write("next to" + age+ " " + "is"+ " " +(age+1));
-
-//var num1 = parseInt(prompt("enter  num1"));
-//var num2 = parseInt(prompt("enter  num2"));
-
-//document.write("the sum " + (num1 + num2));
-
-//document.write("<br/>");
-
-//document.write("the machpela " + (num1 * num2));
-//document.write("<br/>");
-//if (num1 > num2)
-//{
-//    document.write("the efresh " + (num1 - num2));
-//}
-//else if (num2 > num1)
-//{
-//    document.write("the efresh " + (num2 - num1));
-//}
-
-//function GetAnswer() {
-//    var num = parseInt(document.getElementById("num").value);
-//    var num2 = parseInt(document.getElementById("num2").value);
-//    var correct = num + num2;
-//    document.getElementById("num3").value = correct;
-
-//    if (correct = num1 + num2); {
-//        document.getElementById("span").innerHTML = "מעולה"
-//    }
-//    if (correct = !num1 + num2) {
-//        document.getElementById("span").innerHTML = "טעות"
-//    }
-//}
 function CheckAnswer()
 {
     var num = parseInt(document.getElementById("num+").value);
