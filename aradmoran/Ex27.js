777f4b5d5db3a9e670eef2809c98635885375154
arad enoshi <aradenoshi10@gmail.com>
2026-06-01 23:04:50 +0300

    finished 5 targil 1 2 3

---
+++ aradmoran/Ex27.js
@@ -0,0 +1,21 @@
+﻿//targil 1
+function delet() {
+    document.getElementById("img1").style.display = "none";
+}
+function create() {
+    document.getElementById("img1").style.display = "block";
+}
+//targil 2
+function big() {
+    document.getElementById("img2").width += 10;
+
+}
+function small() {
+    document.getElementById("img2").height -= 10;
+}
+//targil 3
+function change() {
+    var change = document.getElementById("img3").src;
+    document.getElementById("img3").src = document.getElementById("img4").src;
+    document.getElementById("img4").src = change;
+}
