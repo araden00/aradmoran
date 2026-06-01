6dfd59853a6320e2385b6cec48064471b3c54c83
arad enoshi <aradenoshi10@gmail.com>
2026-06-01 16:55:01 +0300

    finish targil 2 b targil 7

---
+++ aradmoran/Ex24.js
@@ -0,0 +1,60 @@
+﻿// targil 2 b
+
+//function DrawImage(index, el)
+//{
+//    var img1 = document.createElement("img");
+//    img1.setAttribute("src", "ing/jpg." + index + ".jpg");
+//    img1.setAttribute("width", "100");
+//    img1.setAttribute("height", "100");
+//    var el = document.getElementById("tblText");
+//    el.appendChild(img1);
+//}
+//function Drawntable(arr) {
+//    for (var i = 0; i < arr.length; i++) {
+//        var tabledata1 = document.createElement("td");
+//        DrawImage(arr[i], tabledata1);
+//        var el = document.getElementById("tblText");
+//        el.appendChild(tabledata1);
+//    }
+//}
+//Drawntable([2, 2, 2, 2, 2]);
+
+//targil 7
+function buildTable(arr) {
+    // תיקון ה-ID ל-tbl3
+    var table = document.getElementById("tbl3");
+
+    var row1 = document.createElement("tr");
+    var row2 = document.createElement("tr");
+
+    for (let i = 0; i < arr.length; i += 3) {
+        var text = arr[i];
+        var imgSrc = arr[i + 1];
+        var size = arr[i + 2];
+
+        // טיפול בשורה הראשונה (טקסט)
+        var td1 = document.createElement("td");
+        td1.textContent = text; // התיקון כאן: הוספת הטקסט לתוך התא!
+        row1.appendChild(td1);
+
+        // טיפול בשורה השנייה (תמונה)
+        var td2 = document.createElement("td");
+        var img = document.createElement("img");
+        img.src = imgSrc;
+        img.alt = text;
+        img.width = size;
+        img.height = size;
+        td2.appendChild(img);
+        row2.appendChild(td2);
+    }
+
+    // הוספת השורות המוכנות לטבלה
+    table.appendChild(row1);
+    table.appendChild(row2);
+}
+buildTable([
+    "dolphin", "ing/jpg.1.jpg", "100",
+    "elephant", "ing/jpg.2.jpg", "100",
+    "rabbit", "ing/jpg.3.jpg", "100",
+    "giraffe", "ing/jpg.4.jpg", "100"
+]);
--- aradmoran/targilim.js
+++ aradmoran/targilim.js
@@ -1,37 +1,91 @@
-﻿alert("something");
-document.write("something");
+﻿//alert("something");
+//document.write("something");
 
-document.write(" ");
+//document.write(" ");
 
-var num = prompt("enter a number", 7);
-document.write(num);
+//var num = prompt("enter a number", 7);
+//document.write(num);
 
-document.write(" ");
+//document.write(" ");
 
-var name = prompt("enter your name");
-document.write("hello " + name);
+//var name = prompt("enter your name");
+//document.write("hello " + name);
 
-document.write(" ");
+//document.write(" ");
 
-var age = parseInt(prompt("enter your age", 0));
-document.write("you are " + age + " years old");
+//var age = parseInt(prompt("enter your age", 0));
+//document.write("you are " + age + " years old");
 
-document.write("<br/>");
+//document.write("<br/>");
 
-document.write("next to" + age + " " + "is" + " " + (age + 1));
+//document.write("next to" + age + " " + "is" + " " + (age + 1));
 
-var num1 = parseInt(prompt("enter  num1"));
-var num2 = parseInt(prompt("enter  num2"));
+//var num1 = parseInt(prompt("enter  num1"));
+//var num2 = parseInt(prompt("enter  num2"));
 
-document.write("the sum " + (num1 + num2));
+//document.write("the sum " + (num1 + num2));
 
-document.write("<br/>");
+//document.write("<br/>");
 
-document.write("the machpela " + (num1 * num2));
-document.write("<br/>");
-if (num1 > num2) {
-    document.write("the efresh " + (num1 - num2));
+//document.write("the machpela " + (num1 * num2));
+//document.write("<br/>");
+//if (num1 > num2) {
+//    document.write("the efresh " + (num1 - num2));
+//}
+//else if (num2 > num1) {
+//    document.write("the efresh " + (num2 - num1));
+//}
+//var grade = [90, 80, 70, 60, 50]
+//function avg(grade) {
+//    var sum = 0;
+//    for (var i = 0; i < grade.length; i++) {
+//        sum += grade[i];
+//    }
+//    alert("the avg is " + (sum / grade.length));
+//}
+//function createbutton() {
+//    var button = document.createElement("input");
+//    button.setAttribute("type", "button");
+//    button.setAttribute("value", "click");
+//    button.setAttribute("onclick", "avg(grade)");
+//    var div = document.getElementById("123");
+//    div.appendChild(button);
+//}
+//createbutton();
+//function DrawImage(index, el)
+//{
+//    var img1 = document.createElement("img");
+//    img1.setAttribute("src", "ing/jpg." + index + ".jpg");
+//    img1.setAttribute("width", "100");
+//    img1.setAttribute("height", "100");
+//    var el = document.getElementById("tblText");
+//    el.appendChild(img1);
+//}
+//function Drawntable(arr) {
+//    for (var i = 0; i < arr.length; i++) {
+//        var tabledata1 = document.createElement("td");
+//        DrawImage(arr[i], tabledata1);         
+//        var el = document.getElementById("tblText");
+//        el.appendChild(tabledata1);
+//    }
+//}
+//Drawntable([1, 2, 3, 4, 5]);
+function marchmilim(arr) {
+    var el = document.getElementById("tbl3");
+    var tr = document.createElement("tr");
+    for (var i = 0; i < arr.length/3; i++) {
+        if (i % 3 == 0&&i!=0) {
+            var tr = document.createElement("tr");
+            el.appendChild(tr);
+        }
+        else 
+        {
+            var td = document.createElement("td");
+            td.innerText = arr[i];
+            tr.appendChild(td);
+            
+        }
+        el.appendChild(tr);
+    }
 }
-else if (num2 > num1) {
-    document.write("the efresh " + (num2 - num1));
-}
+marchmilim(["headline","no", "yes", "yep", "clear", "clean", "cloor","dolls","bills"]);
